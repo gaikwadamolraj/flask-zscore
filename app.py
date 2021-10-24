5e9d2a91598e3e4744a06695f2f52560c59157a7
Amol <gaikwadamolraj@gmail.com>
2021-10-24 05:00:00 +0100

    Removed db related code

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
 from flask import Flask, request, jsonify
 
-
-
-from . import config
-
-
 app = Flask(__name__)
-app.app_context().push()
-db.init_app(app)
+
 
 is_db=False
 
@@ -31,9 +25,6 @@ def calculateZscore(data):
 
     return { "year": year, "zscore": (1.2*X1 + 1.4*X2 + 3.3*X3 + 0.6*X4 + 1.0*X5)}  
 
-# save score to db
-def saveZscore(isoCode, companyId, score):
-    return database.add_instance(Score, isoCode=isoCode, companyId=companyId, score=score)
 
 @app.route('/',  methods=['GET'])
 def hello():
@@ -47,29 +38,8 @@ def zScore(isoCode, id):
         zscore = calculateZscore(d)
         zscores.append(zscore)
 
-    if is_db == "True":
-        print('i am inside')
-        # save score to db and return the report id
-        reportId = saveZscore(isoCode, id, zscores)
-        return jsonify({"scores": zscores, "reportId" : reportId})
-    else:
-        print('i am inside else', config.is_db)
-        return jsonify({"scores": zscores})
+    return jsonify({"scores": zscores})
 
 @app.route('/api/v1/company/<string:isoCode>/<int:id>/<int:reportId>', methods=['GET'])
 def getLastReportById(isoCode, id, reportId):
-    if is_db == "True":
-        reports = database.get_by_args(Score, id=reportId)
-        zScoreReport = []
-        for report in reports:
-            if(id == report.companyId and isoCode == report.isoCode):
-                zReport =  {
-                        "id": report.id,
-                        "isoCode": report.isoCode,
-                        "companyId": report.companyId,
-                        "score": report.score
-                    }
-                zScoreReport.append(zReport)
-        return jsonify(zScoreReport), 200
-    else:
-        return jsonify({"message": "Not connected with db, so no prev report"})
+    return jsonify({"message": "Not connected with db, so no prev report"})
